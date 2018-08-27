183b475ccb0fcfe3ac63f92afdd71de90e5b235a
yuyilei <yyl1352091742@gmail.com>
2018-08-27 10:04:12 +0800

    fix: update mongodb set and collection to env

--- service/mongoDB.py
+++ service/mongoDB.py
@@ -3,10 +3,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGODB_HOST = os.getenv('MONGODB_HOST') or 'localhost'
 MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
-
+WEEKSET = os.getenv('WEEKSET')
+WEEKDB = os.getenv('WEEKDB')
 
 async def db_setup():
     client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-    all_week = client['all_week2018']
-    weekdaydb = all_week['weekdaydb2018']
+    all_week = client[WEEKSET]
+    weekdaydb = all_week[WEEKDB]
     return weekdaydb
