1f92035e67e90db2cbcfcd77d4de740842121589
Darren <darren@Darren-MBP.lan>
2019-05-19 01:49:04 +0800

    fix: add mongodb auth

--- service/mongoDB.py
+++ service/mongoDB.py
@@ -3,11 +3,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGODB_HOST = os.getenv('MONGODB_HOST') or 'localhost'
 MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
 WEEKSET = os.getenv('WEEKSET')
 WEEKDB = os.getenv('WEEKDB')
 
 async def db_setup():
-    client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+    # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+    mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+    client = AsyncIOMotorClient(mongo_uri)
     all_week = client[WEEKSET]
     weekdaydb = all_week[WEEKDB]
     return weekdaydb
