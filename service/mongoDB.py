c6aaaa43df0a6a43040ffe6bfab1c42ef7ae704b
Humbertzhang <504490160@qq.com>
2020-01-26 16:14:30 +0800

    Update mongoDB.py

--- service/mongoDB.py
+++ service/mongoDB.py
@@ -1,16 +1,13 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGODB_HOST = os.getenv('MONGODB_HOST') or 'localhost'
-MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
-MONGODB_USERNAME = os.getenv('MONGODB_USERNAME') or "muxi"
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD') or "nopassword"
+MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://username:secret@localhost:27017/?authSource=admin"
 WEEKSET = os.getenv('WEEKSET')
 WEEKDB = os.getenv('WEEKDB')
 
 async def db_setup():
     # client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-    mongo_uri = "mongodb://{}:{}@{}:{}".format(MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT)
+    mongo_uri = MONGO_URI
     client = AsyncIOMotorClient(mongo_uri)
     all_week = client[WEEKSET]
     weekdaydb = all_week[WEEKDB]
